a46312f4849284a69bcd2042702df74fb34b5693
ariarijp <takuya.arita@gmail.com>
2018-09-24 20:43:27 +0900

    Add tests

---
+++ test/converter_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+require 'time'
+require 'embulk/input/redash/converter'
+
+Converter = Embulk::Input::Redash::Converter
+
+class ConverterTest < Test::Unit::TestCase
+  data(
+    'boolean': [true, :boolean, true],
+    'long': [123, :long, 123],
+    'timestamp': [Time.parse('2018-09-24 20:15:45'), :timestamp, '2018-09-24 20:15:45'],
+    'double': [1.23, :double, 1.23],
+    'string': ['embulk', :string, 'embulk'])
+
+  def test_convert(data)
+    expected, type, value = data
+    assert_equal(expected, Converter.convert(type, value))
+  end
+end
